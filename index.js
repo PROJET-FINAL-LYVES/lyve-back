6838a32b02d3860df59accaae5548cc0fc696636
Mathieu <mbriand@2emotion.com>
2023-07-04 10:41:12 +0200

    JsonWebToken auth

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 
 // dependencies
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const express = require('express');
+const cors = require('cors');
 const http = require('http');
 const { Server } = require('socket.io');
 
@@ -16,16 +17,17 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000", // specify the domain of the client
-        methods: ["GET", "POST"]
+        origin: 'http://localhost:3000', // specify the domain of the client
+        methods: ['GET', 'POST']
     }
 });
 
 // controllers
 const userController = require('./controllers/user');
 const videoController = require('./controllers/video');
-const cors = require('cors');
 
+// helpers
+const { verifyJsonWebToken } = require('./helpers');
 
 app.use(cors());
 app.use(express.json());
@@ -39,6 +41,7 @@ mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTo
         console.error('Database connection error');
     });
 
+// TODO: remove later I guess :)
 app.get('/', (req, res) => {
     return res.json({ success: true });
 });
@@ -48,16 +51,14 @@ app.post('/login', userController.login);
 app.post('/register', userController.register);
 app.get('/logout', userController.logout);
 
-// Use the middleware function for protecting sensitive routes
-app.get('/dashboard', userController.requireAuth, (req, res) => {
+app.get('/dashboard', verifyJsonWebToken, (req, res) => {
     res.render('dashboard');
 });
 
-
 let videoStartTime = null;
-
 let currentVideoId = null;
 
+// TODO: verify jwt socket
 io.on('connection', (socket) => {
     console.log('a user connected');
 
