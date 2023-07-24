4ef318cd0e1e5d8b5acb3ebda6be0a20480f794d
Sam <samo.man@hotmail.fr>
2023-07-24 18:30:10 +0200

    WIP :: sync

--- index.js
+++ index.js
@@ -37,11 +37,6 @@ mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTo
         console.error('Database connection error');
     });
 
-// TODO: remove later I guess :)
-app.get('/', (req, res) => {
-    return res.json({ success: true });
-});
-
 // USER ROUTES
 app.post('/login', userController.login);
 app.post('/register', userController.register);
@@ -56,44 +51,52 @@ const rooms = {};
 const playlists = {};
 const playerStates = {};
 
-// TODO: verify jwt socket
 io.on('connection', (socket) => {
     socket.on('join room', (roomId) => {
+        console.log('Nouvel utilisateur ' + roomId);
         socket.join(roomId);
-        console.log('A user joined room ' + roomId + '!');
-        console.log(playerStates[roomId])
 
-
-        // Si la room n'a pas encore de liste de sockets, on la crée
+        // Si la room n'a pas encore de liste de clients, on la crée
         if (!rooms[roomId]) {
+            console.log('Creating new room ' + roomId);
+            playerStates[roomId] = 'paused';  // Initialize playerStates[roomId] when creating a new room
             rooms[roomId] = [];
         }
 
-        // On ajoute la nouvelle socket à la liste si elle n'y est pas déjà
+        // On ajoute le nouveau client à la liste si il n'y est pas déjà
         if (!rooms[roomId].includes(socket.id)) {
             rooms[roomId].push(socket.id);
         }
 
-        console.log('Room ' + roomId + ' has sockets: ' + rooms[roomId]);
         // Si la room n'a pas encore d'hôte, la première socket devient l'hôte
         if (!hosts[roomId]) {
             hosts[roomId] = rooms[roomId][0];
             console.log('Host of room ' + roomId + ' is ' + hosts[roomId]);
         }
 
+        // si le socket n'est pas l'hote, il demande l'état du player
+        if (socket.id !== hosts[roomId]) {
+            socket.to(hosts[roomId]).emit('get player state', socket.id);
+            console.log('Asking host of room ' + roomId + ' for player state');
+        }
+
+        // Send the current video URL if available
+        if (playlists[roomId] && playlists[roomId].length > 0) {
+            console.log('Sending video to ' + socket.id);
+            const currentVideoId = playlists[roomId][0];
+            console.log('Current video: ' + currentVideoId);
+            socket.emit('set video url', currentVideoId);  // Use socket.emit instead of socket.to
+        }
+
+        socket.emit('update playlist', playlists[roomId] || []);
         socket.emit('host status', socket.id === hosts[roomId]);
-
-        // Vérifie si une vidéo est actuellement en cours de lecture.
-        if (playerStates[roomId] === 'playing') {
-            // On demande à l'hôte d'obtenir le temps courant de la vidéo et de l'envoyer au nouvel utilisateur.
-            socket.to(hosts[roomId]).emit('get current time', socket.id);
-        } 
-
-        socket.on('send current time', (newUserId, currentTime) => {
-            socket.to(newUserId).emit('set current time', currentTime);
-        });
-
         io.to(roomId).emit('room users', rooms[roomId]);
+    });
+
+
+    socket.on('send player state', (newUserId, currentTime, playerState) => {
+        socket.to(newUserId).emit('edit client player state', currentTime, playerState);
+        console.log('Sent current state to [' + newUserId + '] (time: ' + currentTime + ', state: ' + playerState + ')');
     });
 
     socket.on('get room users', (roomId) => {
@@ -105,33 +108,27 @@ io.on('connection', (socket) => {
         io.to(roomId).emit('chat message', msg);
     });
 
-    socket.on('video action', (roomId, action) => {
+    const videoActionHandler = (roomId, action) => {
         const currentTime = action.time;
         if (socket.id === hosts[roomId]) {
-            if (action.type === 'play') {
+            if (action.type === 'play' || action.type === 'pause') {
+                playerStates[roomId] = (action.type === 'play') ? 'playing' : 'paused';
                 action.time = currentTime;
-                console.log('Received video action: ', action);
-                socket.broadcast.to(roomId).emit('video action', action, currentTime);
-            } else if (action.type === 'pause') {
-                action.time = currentTime;
-                console.log('Received video action: ', action);
-                socket.broadcast.to(roomId).emit('video action', action, currentTime);
+                socket.broadcast.to(roomId).emit('client video action', action, currentTime);
             }
         }
-    });
+    };
+    socket.on('video action', videoActionHandler);
 
     socket.on('video start', (roomId) => {
-        playerStates[roomId] = 'playing';
+        playerStates[roomId] = 'play';
     });
 
-    socket.on('video end', (roomId) => {
-        playerStates[roomId] = 'idle';
-
+    socket.on('get video url', (roomId) => {
         if (playlists[roomId] && playlists[roomId].length > 0) {
-            const nextVideo = playlists[roomId].shift();
-
-            io.to(roomId).emit('update playlist', playlists[roomId]);
-            io.to(roomId).emit('play video', nextVideo);
+            console.log('Sending video to ' + socket.id);
+            const currentVideoId = playlists[roomId][0];
+            socket.to(socket.id).emit('set video url', currentVideoId);
         }
     });
 
@@ -142,46 +139,70 @@ io.on('connection', (socket) => {
         playlists[roomId].push(videoUrl);
 
         io.to(roomId).emit('update playlist', playlists[roomId]);
-        console.log('Playlist of room ' + roomId + ' is now: ' + playlists[roomId]);
-        if (socket.id === hosts[roomId] && (playlists[roomId].length === 1 || playerStates[roomId] === 'idle')) {
-            io.to(roomId).emit('play video', videoUrl);
+
+        if (playlists[roomId].length === 1) {
+            io.to(roomId).emit('set video url', videoUrl);
         }
     });
 
     socket.on('next video', (roomId) => {
+        console.log('Next video in room ' + roomId);
         if (playlists[roomId] && playlists[roomId].length > 0) {
             const nextVideo = playlists[roomId].shift();
-            console.log('Playlist of room ' + roomId + ' is now: ' + playlists[roomId])
-            console.log('Next video is ' + nextVideo)
             io.to(roomId).emit('update playlist', playlists[roomId]);
-
-            socket.broadcast.to(roomId).emit('video action', 'play', 0);
-
+            io.to(roomId).emit('play video', nextVideo);
+            console.log('Playing next video in room ' + roomId + ': ' + nextVideo);
         }
     });
 
+    socket.on('clear playlist', (roomId) => {
+        if (socket.id === hosts[roomId]) {  // Only allow host to clear the playlist
+            // Keep the first video in the playlist
+            if (playlists[roomId] && playlists[roomId].length > 0) {
+                playlists[roomId] = [playlists[roomId][0]];
+            } else {
+                playlists[roomId] = [];
+            }
+            io.to(roomId).emit('update playlist', playlists[roomId]);
+            console.log(`Host of room ${roomId} cleared the playlist but kept the first video.`);
+        }
+    });
+
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
-        // Pour chaque room
+        // For each room
         for (const roomId in rooms) {
-            // On trouve l'index de la socket dans la liste
+            // Find the index of the socket in the list
             const index = rooms[roomId].indexOf(socket.id);
             if (index > -1) {
-                // On retire la socket de la liste
+                // Remove the socket from the list
                 rooms[roomId].splice(index, 1);
-
-                // Si la socket était l'hôte, on attribue l'hôte à la prochaine socket dans la liste
+                console.log('Removed user ' + socket.id + ' from room ' + roomId);
+                // If the socket was the host, assign the host to the next socket in the list
                 if (hosts[roomId] === socket.id) {
                     console.log('Host of room ' + roomId + ' is now ' + rooms[roomId][0]);
                     hosts[roomId] = rooms[roomId][0];
 
                     // Send host status update to the new host
                     io.to(hosts[roomId]).emit('host status', true);
-                    io.to(roomId).emit('room users', rooms[roomId]);
+                }
+
+                // Always send updated user list
+                io.to(roomId).emit('room users', rooms[roomId]);
+
+                // Check if the room is empty and if so, clear the playlist
+                if (rooms[roomId].length === 0) {
+                    console.log(`Room ${roomId} is empty, clearing playlist.`);
+                    playlists[roomId] = [];
+                    io.to(roomId).emit('update playlist', playlists[roomId]);
+                    delete hosts[roomId];  // Delete host entry if room is empty
+                    delete playerStates[roomId];  // Delete player state entry if room is empty
                 }
             }
         }
     });
+
 });
 
 server.listen(port, () => {
